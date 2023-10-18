c9436425aad82c26142177e166de59f8db6b7ec9
broms95 <aptuner@gmail.com>
2023-10-17 21:38:06 -0500

    Add files via upload

---
+++ FileProcessor.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace DriveScannerNET
+{
+    internal class FileProcessor
+    {
+        private readonly static string[] patterns = new string[] {
+//            ".000000000.",            // compact SSN
+              ".000-00-0000.",          // expanded SSN
+//            ".0000000000000000.",     // compact CC
+//            ".0000.0000.0000.0000."   // expanded CC
+        };
+
+        private readonly static char[] lookupTable = initializeLookupTable();
+        private const int OVERLAP_SIZE = 32;
+        private const int BUFFER_SIZE = 4096;
+
+        private byte[] readBuffer = new byte[BUFFER_SIZE];
+        private byte[] textBuffer = new byte[BUFFER_SIZE + OVERLAP_SIZE];
+        private StringBuilder ifBuffer = new StringBuilder(BUFFER_SIZE + OVERLAP_SIZE);
+        private String currentPath = "";
+
+        public Int64 processWorkItem(String path)
+        {
+            Int64 bytesProcessed = 0;
+            Array.Fill(textBuffer, (byte)' ');
+ 
+            // setup for the next file
+            currentPath = path;
+
+            using (var sr = new FileStream(path, FileMode.Open, FileAccess.Read, FileShare.None, BUFFER_SIZE))
+            {
+                int bytesRead;
+                while ((bytesRead = sr.Read(readBuffer, 0, BUFFER_SIZE)) > 0)
+                {
+                    bytesProcessed += bytesRead;
+                    processWorkItemBuffer(bytesRead);
+                }
+
+            }
+
+            return bytesProcessed;
+        }
+
+        protected void processWorkItemBuffer(int readBufferSize)
+        {
+            // prepare the text buffer.  We do this by shifting the end
+            // to the beginning and copying in the new data
+            Array.Copy(textBuffer, BUFFER_SIZE, textBuffer, 0, OVERLAP_SIZE);
+            Array.Copy(readBuffer, 0, textBuffer, OVERLAP_SIZE, readBufferSize);
+            int textBufferValid = OVERLAP_SIZE + readBufferSize;
+
+            ifBuffer.Length = textBufferValid;
+            for (int i = 0; i < textBufferValid; i++)
+            {
+                ifBuffer[i] = lookupTable[textBuffer[i]];
+            }
+
+            // for now we are only searching for expanded SSN but in the future
+            // we can search for all sorts of patterns like CC numbers of different
+            // formats.  See design doc for more.
+            //
+            // We might want to bite the bullet and use Regular expressions here
+            // but this might be too slow.  Would need to profile once we know
+            // what type of patterns we want to look for and how expensive they are.
+            var stringToSearch = ifBuffer.ToString(0, textBufferValid);
+            foreach (var pattern in patterns)
+            {
+                int position = 0;
+                for (; ; )
+                {
+                    position = stringToSearch.IndexOf(pattern, position, StringComparison.Ordinal);
+                    if (position == -1)
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        // extract the text from the ifBuffer
+                        var actualText = Encoding.UTF8.GetString(textBuffer, position + 1, pattern.Length - 2 );
+                        Console.WriteLine(currentPath + ": " + actualText);
+                        position++;
+                    }
+                }
+            }
+        }
+
+        static protected char[] initializeLookupTable()
+        {
+            // convert the text buffer into intermediate form
+            char[] table = new char[byte.MaxValue + 1];
+            Array.Fill(table, '.');
+            table['0'] = '0';
+            table['1'] = '0';
+            table['2'] = '0';
+            table['3'] = '0';
+            table['4'] = '0';
+            table['5'] = '0';
+            table['6'] = '0';
+            table['7'] = '0';
+            table['8'] = '0';
+            table['9'] = '0';
+            table['-'] = '-';
+            return table;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,144 @@
+﻿using System;
+
+namespace DriveScannerNET
+{
+    internal class Program
+    {
+
+        Queue<String> workItems = new Queue<String>();
+        int dirsScanned = 0;
+        int filesScanned = 0;
+        long bytesScanned = 0;
+        int workItemsProcessed = 0;
+
+        void generateWorkItems(String startingPath)
+        {
+            // use stack instead of recursviely calling into generateWorkItems.
+            // its more efficient and we can early return once we hit 100,000 items.
+            var dirs = new Queue<String>();
+            dirsScanned++;
+            dirs.Enqueue(startingPath);
+
+            while(dirs.Count != 0)
+            {
+                var path = dirs.Dequeue();
+                try
+                {
+                    foreach (string f in Directory.GetFiles(path))
+                    {
+                         long fileSize = new FileInfo(f).Length;
+
+                        // We don't scan files longer than 1 GB for now.
+                        if (fileSize > (1024 * 1024 * 1024)) continue;
+
+                        bytesScanned += fileSize;
+                        filesScanned++;
+                        workItems.Enqueue(f);
+                        if (workItems.Count == 100000)
+                        {
+                            return;
+                        }
+                    }
+                    foreach (string d in Directory.GetDirectories(path))
+                    {
+                        dirsScanned++;
+                        dirs.Enqueue(d);
+                    }
+                }
+                catch (System.Exception)
+                {
+                    // eat the exceptions.  There are a lot of reasons we could
+                    // get a failure here (permissions, etc.)
+                }
+            }
+        }
+
+        void processThread()
+        {
+            var processor = new FileProcessor();
+            for(; ; )
+            {
+                String item = "";
+                int processCount;
+                lock(workItems)
+                {
+                    if (workItems.Count == 0) 
+                    {
+                        Console.WriteLine("File processor thread done.");
+                        return;
+                    }
+                    item = workItems.Dequeue();
+                    processCount = ++workItemsProcessed;
+                }
+
+                processor.processWorkItem(item);
+//                Console.WriteLine("Processing " + item);
+
+                if( processCount % 100 == 0)
+                {
+                    Console.WriteLine(processCount);
+                }
+            }
+        }
+
+        void scan()
+        {
+            String pathToScan = "C:\\dev";
+            int threadsToUse = 10;
+
+            var timeStart = Environment.TickCount;
+
+            // generate the list of work items
+            generateWorkItems(pathToScan);
+            //            foreach (string f in workItems) Console.WriteLine(f);
+
+            var timePhase1 = Environment.TickCount;
+
+            var threads = new List<Thread>();
+            for (int i = 0; i < threadsToUse; i++)
+            {
+                var thread = new Thread(processThread);
+                thread.Start();
+                threads.Add(thread);    
+            }
+
+            // the threads will die once there are no work items
+            // left.  Just join each one
+            foreach( var t in threads )
+            {
+                t.Join();
+            }
+
+            var timePhase2 = Environment.TickCount;
+
+            // report findings
+            var gb = bytesScanned / (double)(1024 * 1024 * 1024);
+
+            Console.WriteLine("Threads:                " + threadsToUse );
+            Console.WriteLine("Directories scanned:    " + dirsScanned );
+            Console.WriteLine("Files scanned:          " + filesScanned );
+            Console.WriteLine("Bytes scanned:          " + gb.ToString("0.###") + " GB" );
+
+            // report the timings
+            double durationPhase1 = (timePhase1 - timeStart) / 1000.0;
+            Console.WriteLine("Time for phase 1:       " + durationPhase1 + " sec");
+
+            double durationPhase2 = (timePhase2 - timePhase1) / 1000.0;
+            Console.WriteLine("Time for phase 2:       " + durationPhase2 + " sec");
+
+            double totalTime = (timePhase2 - timeStart) / 1000.0;
+            Console.WriteLine("Total scanning time:    " + totalTime + " sec");
+
+            double speed = gb / totalTime;
+            double speedPerThread = speed / threadsToUse;
+            Console.WriteLine("Bandwidth (per thread): " + speedPerThread.ToString("0.###") + " GB/sec");
+            Console.WriteLine("Bandwidth (aggregate):  " + speed.ToString("0.###") + " GB/sec");
+        }
+
+        static void Main(string[] args)
+        {
+            Program prog = new Program();
+            prog.scan();
+        }
+    }
+}
